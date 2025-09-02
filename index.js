945e98822ee298ede9fcc19f12d291963828961b
chaitanya <psaichaitanya27@gmail.com>
2025-09-02 15:50:50 +0530

    redux-setup-current-user

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import dbconnect from "./config/dbconnect.js"
 import cookieParser from "cookie-parser"
 import authRouter from "./routes/authroutes.js"
 import cors from "cors"
+import userRouter from "./routes/userroutes.js"
 const port = process.env.PORT || 5000
 const app=express()
 app.use(cors({
@@ -14,8 +15,7 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth",authRouter)
-
-
+app.use("/api/user",userRouter)
 app.get("/",(req,res)=>{
     res.send("Hello")
 })
---
+++ middleware/isAuth.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken'
+
+const isAuth = async(req,res,next)=>{
+    try {
+        const token = req.cookies.token
+        if(!token){
+            return res.status(400).json({message:'Token is required'}) 
+        }
+        const decodeToken = jwt.verify(token,process.env.JWT_SECRET)
+        if(!decodeToken){
+            return res.status(400).json({message:'Token not verified'})
+        } 
+        req.userId=decodeToken.userId
+        next()
+    } catch (error) {
+        return res.status(500).json({message:`isauth error ${error}`})
+
+    }
+}
+
+export default isAuth
