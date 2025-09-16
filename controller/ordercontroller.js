9f191ace4ff14afae0ec1c34dbe679db9ae13178
chaitanya <psaichaitanya27@gmail.com>
2025-09-16 20:21:57 +0530

    getOrders

--- controller/ordercontroller.js
+++ controller/ordercontroller.js
@@ -1,51 +1,79 @@
 import Order from '../model/ordermodel.js'
 import Shop from '../model/shopmodel.js'
-export const placeorder = async(req,res)=>{
-    try{
-        const {cartItems,paymentMethod,deliveryAddress,totalAmount} = req.body
-        if(cartItems.length == 0 || !cartItems){
-            return res.status(400).json({message:'Cart is empty'})
+import User from '../model/usermodel.js'
+
+export const placeorder = async (req, res) => {
+    try {
+        const { cartItems, paymentMethod, deliveryAddress, totalAmount } = req.body
+        if (cartItems.length == 0 || !cartItems) {
+            return res.status(400).json({ message: 'Cart is empty' })
         }
-        if(!deliveryAddress.text || !deliveryAddress.latitude || !deliveryAddress.longitude){
-            return res.status(400).json({message:'send complete address'})
+        if (!deliveryAddress.text || !deliveryAddress.latitude || !deliveryAddress.longitude) {
+            return res.status(400).json({ message: 'send complete address' })
         }
-        const groupItemsByShop ={}
+        const groupItemsByShop = {}
 
         cartItems.forEach(item => {
             const shopId = item.shop
-            if(!groupItemsByShop[shopId]){
-                groupItemsByShop[shopId]=[]
+            if (!groupItemsByShop[shopId]) {
+                groupItemsByShop[shopId] = []
             }
             groupItemsByShop[shopId].push(item)
         });
-        const shopOrders = await Promise.all(Object.keys(groupItemsByShop).map(async(shopId)=>{
+        const shopOrders = await Promise.all(Object.keys(groupItemsByShop).map(async (shopId) => {
             const shop = await Shop.findById(shopId).populate("owner")
-            if(!shop){
-                return res.status(400).json({message:'shop not found'})
+            if (!shop) {
+                return res.status(400).json({ message: 'shop not found' })
             }
             const items = groupItemsByShop[shopId]
-            const subtotal = items.reduce((sum,i)=>sum+Number(i.price)*Number(i.quantity),0)
+            const subtotal = items.reduce((sum, i) => sum + Number(i.price) * Number(i.quantity), 0)
             return {
-                shop:shop._id,
-                owner:shop.owner.id,
+                shop: shop._id,
+                owner: shop.owner.id,
                 subtotal,
-                shopOrderItems:items.map((i)=>({
-                    item:i.id,
-                    name:i.name,
-                    price:i.price,
-                    quantity:i.quantity
+                shopOrderItems: items.map((i) => ({
+                    item: i.id,
+                    name: i.name,
+                    price: i.price,
+                    quantity: i.quantity
                 }))
             }
         }))
         const newOrder = await Order.create({
-            user:req.userId,
+            user: req.userId,
             paymentMethod,
             deliveryAddress,
             totalAmount,
             shopOrders
         })
         return res.status(201).json(newOrder)
-    }catch(error){
-        return res.status(500).json({message:`place order error ${error}`})
+    } catch (error) {
+        return res.status(500).json({ message: `place order error ${error}` })
     }
 }
+
+export const getMyOrders = async (req, res) => {
+    try {
+        const user = await User.findById(req.userId)
+        if (user.role == "user") {
+            const orders = await Order.find({ user: req.userId })
+                .sort({ createdAt: -1 })
+                .populate("shopOrders.shop", "name")
+                .populate("shopOrders.owner", "name email number")
+                .populate("shopOrders.shopOrderItems.item", "name image price")
+
+            return res.status(200).json(orders)
+        } else if(user.role == "owner"){
+            const orders = await Order.find({ "shopOrders.owner": req.userId })
+                .sort({ createdAt: -1 })
+                .populate("shopOrders.shop", "name")
+                .populate("user")
+                .populate("shopOrders.shopOrderItems.item", "name image price")
+
+            return res.status(200).json(orders)
+        }
+    } catch (error) {
+        return res.status(500).json({ message: `get user orders error ${error}` })
+    }
+}
+
--- routes/orderroutes.js
+++ routes/orderroutes.js
@@ -1,11 +1,12 @@
 import express from "express"
 import isAuth from "../middleware/isAuth.js"
-import { placeorder } from "../controller/ordercontroller.js"
+import { getMyOrders, placeorder } from "../controller/ordercontroller.js"
 
 
 const orderRouter = express.Router()
 
 orderRouter.post('/placeorder',isAuth,placeorder)
+orderRouter.get('/my-orders',isAuth,getMyOrders)
 
 
 
