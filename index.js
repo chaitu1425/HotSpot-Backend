a43c09b704de00269d3b87501cc6248fb4bc25d5
chaitanya <psaichaitanya27@gmail.com>
2025-09-25 15:52:56 +0530

    added web socket

--- index.js
+++ index.js
@@ -9,8 +9,22 @@ import userRouter from "./routes/userroutes.js"
 import shopRouter from "./routes/shoproutes.js"
 import itemRouter from "./routes/itemroutes.js"
 import orderRouter from "./routes/orderroutes.js"
+import http from 'http'
+import { Server } from "socket.io"
+import { soketHandle } from "./socket.js"
 const port = process.env.PORT || 5000
 const app=express()
+const server = http.createServer(app)
+
+const io = new Server(server,{
+    cors:{
+    origin:"http://localhost:5173",
+    credentials:true,
+    methods:['POST','GET']
+    }
+})
+app.set("io",io)
+
 app.use(cors({
     origin:"http://localhost:5173",
     credentials:true
@@ -22,11 +36,12 @@ app.use("/api/user",userRouter)
 app.use("/api/shop",shopRouter)
 app.use("/api/item",itemRouter)
 app.use("/api/order",orderRouter)
+soketHandle(io)
 app.get("/",(req,res)=>{
     res.send("Hello")
 })
 
-app.listen(8000,()=>{   
+server.listen(8000,()=>{   
     console.log(`Server is running in ${port}`)
     dbconnect()
 })
