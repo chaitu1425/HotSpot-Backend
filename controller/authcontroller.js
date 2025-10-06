f271767acb5f0552be61ee257872f082ffae16c6
chaitanya <psaichaitanya27@gmail.com>
2025-10-06 11:08:08 +0530

    auth controller cookies update

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -28,8 +28,8 @@ export const signUp = async (req, res) => {
 
         const token = await gentoken(user._id)
         res.cookie("token", token, {
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true
         })
@@ -55,8 +55,8 @@ export const signIn = async (req, res) => {
 
         const token = await gentoken(user._id)
         res.cookie("token", token, {
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true
         })
@@ -149,8 +149,8 @@ export const googleAuth = async(req,res)=>{
         }
         const token =await gentoken(user._id) 
         res.cookie("token",token,{
-            secure:false,
-            sameSite:"strict",
+            secure:true,
+            sameSite:"none",
             maxAge:7*24*60*60*1000,
             httpOnly:true
         })
